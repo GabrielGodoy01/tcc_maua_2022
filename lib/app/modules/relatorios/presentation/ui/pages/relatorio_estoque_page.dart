e71ebe7b424648a36eb86427a025c9faac96ff35
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2022-09-22 17:40:12 -0300

    Tab view relatorios

--- lib/app/modules/relatorios/presentation/ui/pages/relatorio_estoque_page.dart
+++ lib/app/modules/relatorios/presentation/ui/pages/relatorio_estoque_page.dart
@@ -12,195 +12,172 @@ class RelatorioEstoquePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
           child: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: const BorderRadius.all(
-              Radius.circular(10),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 7,
-                blurRadius: 10,
-                offset: const Offset(0, 3), // changes position of shadow
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 24),
+                child: Text(
+                  'Relatório de Estoque',
+                  style: TextStyle(fontSize: 36),
+                ),
               ),
-            ],
-          ),
-          height: MediaQuery.of(context).size.height * 0.7,
-          width: MediaQuery.of(context).size.width * 0.7,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(bottom: 24),
-                  child: Text(
-                    'Relatório de Estoque',
-                    style: TextStyle(fontSize: 36),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Expanded(
+                    child: Text(
+                      'Descrição',
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Expanded(
-                      child: Text(
-                        'Descrição',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
+                  Expanded(
+                    child: Text(
+                      'Quantidade',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Expanded(
-                      child: Text(
-                        'Quantidade',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      'Estoque Mín.',
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                     ),
-                    Expanded(
-                      child: Text(
-                        'Estoque Mín.',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
-                      ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      'Estoque Máx.',
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                     ),
-                    Expanded(
-                      child: Text(
-                        'Estoque Máx.',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
-                      ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      'Custo Un.',
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                     ),
-                    Expanded(
-                      child: Text(
-                        'Custo Un.',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
-                      ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      'Localização',
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                     ),
-                    Expanded(
-                      child: Text(
-                        'Localização',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 12,
-                ),
-                const Divider(
-                  color: Colors.black,
-                ),
-                Observer(builder: (_) {
-                  return Flexible(
-                      child: controller.listaItensEstoque.isEmpty
-                          ? const Center(
-                              child: CircularProgressIndicator(
-                                color: Colors.blue,
-                                strokeWidth: 10,
-                              ),
-                            )
-                          : ListView.builder(
-                              itemCount: controller.listaItensEstoque.length,
-                              itemBuilder: (context, index) {
-                                return Column(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 12),
-                                      child: Row(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.center,
-                                        children: [
-                                          Expanded(
-                                            child: Text(
-                                              controller
-                                                  .listaItensEstoque[index]
-                                                  .descricao,
-                                              textAlign: TextAlign.center,
-                                              style: const TextStyle(
-                                                fontSize: 18,
-                                              ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const Divider(
+                color: Colors.black,
+              ),
+              Observer(builder: (_) {
+                return Flexible(
+                    child: controller.listaItensEstoque.isEmpty
+                        ? const Center(
+                            child: CircularProgressIndicator(
+                              color: Colors.blue,
+                              strokeWidth: 10,
+                            ),
+                          )
+                        : ListView.builder(
+                            itemCount: controller.listaItensEstoque.length,
+                            itemBuilder: (context, index) {
+                              return Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 12),
+                                    child: Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Expanded(
+                                          child: Text(
+                                            controller.listaItensEstoque[index]
+                                                .descricao,
+                                            textAlign: TextAlign.center,
+                                            style: const TextStyle(
+                                              fontSize: 18,
                                             ),
                                           ),
-                                          Expanded(
-                                            child: Text(
-                                              controller
-                                                  .listaItensEstoque[index]
-                                                  .quantidade
-                                                  .toString(),
-                                              textAlign: TextAlign.center,
-                                              style: const TextStyle(
-                                                fontSize: 18,
-                                              ),
+                                        ),
+                                        Expanded(
+                                          child: Text(
+                                            controller.listaItensEstoque[index]
+                                                .quantidade
+                                                .toString(),
+                                            textAlign: TextAlign.center,
+                                            style: const TextStyle(
+                                              fontSize: 18,
                                             ),
                                           ),
-                                          Expanded(
-                                            child: Text(
-                                              controller
-                                                  .listaItensEstoque[index]
-                                                  .estoqueMinimo
-                                                  .toString(),
-                                              textAlign: TextAlign.center,
-                                              style:
-                                                  const TextStyle(fontSize: 18),
-                                            ),
+                                        ),
+                                        Expanded(
+                                          child: Text(
+                                            controller.listaItensEstoque[index]
+                                                .estoqueMinimo
+                                                .toString(),
+                                            textAlign: TextAlign.center,
+                                            style:
+                                                const TextStyle(fontSize: 18),
                                           ),
-                                          Expanded(
-                                            child: Text(
-                                              controller
-                                                  .listaItensEstoque[index]
-                                                  .estoqueMaximo
-                                                  .toString(),
-                                              textAlign: TextAlign.center,
-                                              style:
-                                                  const TextStyle(fontSize: 18),
-                                            ),
+                                        ),
+                                        Expanded(
+                                          child: Text(
+                                            controller.listaItensEstoque[index]
+                                                .estoqueMaximo
+                                                .toString(),
+                                            textAlign: TextAlign.center,
+                                            style:
+                                                const TextStyle(fontSize: 18),
                                           ),
-                                          Expanded(
-                                            child: Text(
-                                              'R\$ ${controller.listaItensEstoque[index].custo.toStringAsFixed(2).replaceAll('.', ',')}',
-                                              textAlign: TextAlign.center,
-                                              style:
-                                                  const TextStyle(fontSize: 18),
-                                            ),
+                                        ),
+                                        Expanded(
+                                          child: Text(
+                                            'R\$ ${controller.listaItensEstoque[index].custo.toStringAsFixed(2).replaceAll('.', ',')}',
+                                            textAlign: TextAlign.center,
+                                            style:
+                                                const TextStyle(fontSize: 18),
                                           ),
-                                          Expanded(
-                                            child: Text(
-                                              controller
-                                                      .listaItensEstoque[index]
-                                                      .localizacao ??
-                                                  '...',
-                                              textAlign: TextAlign.center,
-                                              style:
-                                                  const TextStyle(fontSize: 18),
-                                            ),
+                                        ),
+                                        Expanded(
+                                          child: Text(
+                                            controller.listaItensEstoque[index]
+                                                    .localizacao ??
+                                                '...',
+                                            textAlign: TextAlign.center,
+                                            style:
+                                                const TextStyle(fontSize: 18),
                                           ),
-                                        ],
-                                      ),
+                                        ),
+                                      ],
                                     ),
-                                    const Divider(
-                                      color: Colors.black,
-                                    ),
-                                  ],
-                                );
-                              },
-                            ));
-                }),
-              ],
-            ),
+                                  ),
+                                  const Divider(
+                                    color: Colors.black,
+                                  ),
+                                ],
+                              );
+                            },
+                          ));
+              }),
+            ],
           ),
         ),
       )),
--- lib/app/modules/relatorios/presentation/ui/pages/relatorios_page.dart
+++ lib/app/modules/relatorios/presentation/ui/pages/relatorios_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-import 'package:tcc_maua_2022/app/modules/relatorios/presentation/ui/widgets/buttons/relatorios_button_widget.dart';
+import 'package:tcc_maua_2022/app/modules/relatorios/presentation/ui/pages/relatorio_estoque_page.dart';
 
 class RelatoriosPage extends StatelessWidget {
   const RelatoriosPage({super.key});
@@ -8,13 +7,58 @@ class RelatoriosPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RelatoriosButtonWidget(
-          nomeRelatorio: 'Estoque',
-          icon: Icons.grid_on,
-          onPressed: () => Modular.to.navigate('/relatorios/estoque'),
+        body: Center(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(10),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 7,
+              blurRadius: 10,
+              offset: const Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        height: MediaQuery.of(context).size.height * 0.7,
+        width: MediaQuery.of(context).size.width * 0.7,
+        child: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            body: Column(
+              children: [
+                Container(
+                  color: Colors.blue,
+                  child: const TabBar(
+                    labelColor: Colors.white,
+                    indicatorColor: Colors.white,
+                    tabs: [
+                      Tab(
+                        icon: Icon(Icons.grid_on),
+                        text: 'Relatório de Estoque',
+                      ),
+                      Tab(icon: Icon(Icons.change_circle)),
+                      Tab(icon: Icon(Icons.change_circle)),
+                    ],
+                  ),
+                ),
+                const Flexible(
+                  child: TabBarView(
+                    children: [
+                      RelatorioEstoquePage(),
+                      Center(child: CircularProgressIndicator()),
+                      Center(child: CircularProgressIndicator()),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
-    );
+    ));
   }
 }
